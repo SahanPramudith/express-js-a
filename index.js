b41f18feee02207087df3f89ed13205d1db40cc5
sahanpramudith <pramuditsahan466@gmail.com>
2025-04-24 10:36:14 +0530

    feat: add product and user routers to main application

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
+import productRouter from './src/route/product.mjs'
+import userRouter from './src/route/user.mjs'
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/product', productRouter)
+app.use('/user', userRouter)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
